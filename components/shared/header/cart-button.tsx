b01321c1cdfdd10d019745d067c1e109f1cd1e68
Erfan Heshmati <heshmati74@gmail.com>
2025-06-23 11:55:03 +0330

    12-create-cart-sidebar

--- components/shared/header/cart-button.tsx
+++ components/shared/header/cart-button.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import useIsMounted from "@/hooks/use-is-mounted";
 import { cn } from "@/lib/utils";
 import useCartStore from "@/hooks/use-cart-store";
+import useCartSidebar from "@/hooks/use-cart-sidebar";
 
 export default function CartButton() {
   const isMounted = useIsMounted();
@@ -14,6 +15,8 @@ export default function CartButton() {
   } = useCartStore();
 
   const cartItemsCount = items.reduce((a, c) => a + c.quantity, 0);
+
+  const isCartSidebarOpen = useCartSidebar();
 
   return (
     <Link href="/cart" className="px-1 header-button">
@@ -31,6 +34,12 @@ export default function CartButton() {
           </span>
         )}
         <span className="font-bold">Cart</span>
+
+        {isCartSidebarOpen && (
+          <div
+            className={`absolute top-3 -right-4 rotate-[-90deg] z-10 w-0 h-0 border-l-[7px] border-r-[7px] border-b-[8px] border-transparent border-b-background`}
+          ></div>
+        )}
       </div>
     </Link>
   );
